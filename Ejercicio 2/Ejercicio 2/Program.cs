5816491874cd84d52e861498faaf8dc516e9af11
Jorge Asus <jorgehn1995@gmail.com>
2021-02-19 20:54:21 -0600

    Primer Commit

---
+++ Ejercicio 2/Ejercicio 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 2");
+            Console.WriteLine("Ingrese el monto de su venta: ");
+            decimal monto = 0;
+            decimal tasa = 20;
+            decimal descuento = 0;
+            decimal total = 0;
+            if(Decimal.TryParse(Console.ReadLine(), out monto))
+            {
+                if (monto > 300)
+                {
+                    descuento =(monto * tasa) / 100;
+                    total = monto - descuento;
+                }
+                Console.WriteLine("Su tiene un descuento del 20%");
+                Console.WriteLine("Descuento: " + descuento);
+                Console.WriteLine("Total a pagar: " + total);
+            }
+        }
+    }
+}
---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 1");
+            Console.WriteLine("Ingrese una letra");
+            String letra = Console.ReadLine();
+            if (Char.IsUpper(letra, 0))
+            {
+                Console.WriteLine("Es mayuscula");
+            }
+            else
+            {
+                Console.WriteLine("Es minuscula");
+            }
+        }
+    }
+}
